56154355730cfc6193236e47e40688106a1bdfc4
david Anthony <giddydanny002@gmail.com>
2024-11-06 05:06:21 +0000

    Added syntax primitive types

--- app/src/main/java/com/example/welcomekotlin/MainActivity.kt
+++ app/src/main/java/com/example/welcomekotlin/MainActivity.kt
@@ -1,8 +1,23 @@
 package com.example.welcomekotlin
 
 fun main() {
-    //This is my first program
+    //1. This is my first program
     println("Hello David Anthony")
     //Classic Hello World
     println("Hello World!")
+
+    //2. Kotlin Variables
+    // var variableName = value (Integer)
+    //val variableName = value (String)
+    val fullname = "David Anthony-Nted"
+    val age = 21
+    val currentYear : Int = 2003
+    println("$fullname is $age in $currentYear")
+
+    //3. Primitive Integer: Byte
+    val number1 : Byte = 91
+
+    // Primitive Integer: short
+    val number2 : Short = -32000
+    println("$number1 and $number2")
 }
